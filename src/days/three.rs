cd1fcfa501da8c1dab3f6482df689387dfd30e32
Max Perea Düring <maxpereaduring@gmail.com>
2022-12-03 14:52:54 +0100

    Cleanup

--- src/days/three.rs
+++ src/days/three.rs
@@ -2,52 +2,29 @@ use regex::Regex;
 use std::collections::HashSet;
 
 pub fn solution_easy(input: &str) -> i32 {
-    input
-        .lines()
-        .map(|line| {
-            let mut items = HashSet::new();
-            let mut duplicate: Option<char> = None;
-            for (i, x) in line.chars().enumerate() {
-                if i < (line.len() / 2) {
-                    items.insert(x);
-                } else if items.contains(&x) {
-                    duplicate = Some(x);
-                } else {
-                    continue;
-                }
-            }
-            let priority = alpha_to_priority(duplicate.unwrap()).unwrap();
-            priority
-        })
-        .sum()
+    input.lines().map(line_score).sum()
 }
 
 pub fn solution_hard(input: &str) -> i32 {
     let re = Regex::new(r"\w+\n\w+\n\w+").unwrap();
-    let mut sum = 0;
-    for cap in re.captures_iter(input) {
-        sum += badge_score(&cap[0]);
-    }
-    sum
+    re.captures_iter(input)
+        .map(|cap| badge_score(&cap[0]))
+        .sum()
+}
+
+pub fn line_score(line: &str) -> i32 {
+    let (first, second) = line.split_at(line.len() / 2);
+    let first_set: HashSet<_> = first.chars().into_iter().collect();
+    let second_set: HashSet<_> = second.chars().into_iter().collect();
+    alpha_to_priority(*first_set.intersection(&second_set).last().unwrap()).unwrap()
 }
 
 pub fn badge_score(input: &str) -> i32 {
-    let mut group1 = HashSet::new();
-    let mut group2 = HashSet::new();
-    let lines: Vec<&str> = input.lines().collect();
-    for c in lines[0].chars() {
-        group1.insert(c);
-    }
-    for c in lines[1].chars() {
-        group2.insert(c);
-    }
-    for c in lines[2].chars() {
-        if group1.contains(&c) && group2.contains(&c) {
-            return alpha_to_priority(c).unwrap();
-        }
-    }
-
-    0
+    let sets: Vec<HashSet<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let intersection = sets.iter().skip(1).fold(sets[0].clone(), |acc, hs| {
+        acc.intersection(hs).cloned().collect()
+    });
+    alpha_to_priority(*intersection.iter().next().unwrap()).unwrap()
 }
 
 pub fn alpha_to_priority(alpha: char) -> Option<i32> {
