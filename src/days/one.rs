25827e813f819f41b0a37edf81ef5cc4b832e33b
Max Perea Düring <maxpereaduring@gmail.com>
2022-12-03 15:16:15 +0100

    Cleanup

--- src/days/one.rs
+++ src/days/one.rs
@@ -1,29 +1,18 @@
 pub fn solution_easy(input: &str) -> i32 {
-    let calories: Vec<i32> = input
-        .split("\n\n")
-        .map(|group| {
-            group
-                .split("\n")
-                .map(|line| line.to_string().parse::<i32>().unwrap_or(0))
-                .sum::<i32>()
-        })
-        .collect();
-
+    let calories: Vec<i32> = input.split("\n\n").map(read_str).collect();
     *calories.iter().max().unwrap()
 }
 
 pub fn solution_hard(input: &str) -> i32 {
-    let mut calories: Vec<i32> = input
-        .split("\n\n")
-        .map(|group| {
-            group
-                .split("\n")
-                .map(|line| line.to_string().parse::<i32>().unwrap_or(0))
-                .sum::<i32>()
-        })
-        .collect();
-
+    let mut calories: Vec<i32> = input.split("\n\n").map(read_str).collect();
     calories.sort();
     calories.reverse();
     calories[..3].iter().sum()
+}
+
+pub fn read_str(input: &str) -> i32 {
+    input
+        .split("\n")
+        .map(|line| line.to_string().parse::<i32>().unwrap_or(0))
+        .sum::<i32>()
 }
