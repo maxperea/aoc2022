986196f3e7ba2533ac987bd3389086db41385a94
Max Perea Düring <maxpereaduring@gmail.com>
2022-12-24 22:38:14 +0100

    Better parsing

--- src/days/day21.rs
+++ src/days/day21.rs
@@ -11,7 +11,8 @@ pub fn solution_hard(input: &str) -> i64 {
 
     let goal = f_dict(0, &mut dict).1;
     let mut f = |x| f_dict(x, &mut dict).0;
-    let mut x = 3952673900000; // Found using black magic.
+
+    let mut x = 3_952_673_900_000; // Found using black magic.
 
     loop {
         x += 1;
@@ -56,47 +57,40 @@ use nom::{
     bytes::complete::tag,
     character::complete::{alpha1, i64},
     combinator::map,
-    sequence::separated_pair,
+    sequence::{separated_pair, tuple},
     IResult,
 };
 
 fn parse_string(input: &str) -> IResult<&str, String> {
     map(alpha1, String::from)(input)
 }
-fn parse_line(input: &str) -> IResult<&str, (String, Job)> {
-    separated_pair(parse_string, tag(": "), parse_rhs)(input)
+
+fn parse_binary(input: &str) -> IResult<&str, (String, &str, String)> {
+    tuple((
+        parse_string,
+        alt((tag(" + "), tag(" - "), tag(" * "), tag(" / "))),
+        parse_string,
+    ))(input)
 }
+
+fn parse_operation(input: &str) -> IResult<&str, Job> {
+    match parse_binary(input) {
+        Ok((_, (l, " + ", r))) => Ok(("", Addition((l, r)))),
+        Ok((_, (l, " - ", r))) => Ok(("", Subtraction((l, r)))),
+        Ok((_, (l, " * ", r))) => Ok(("", Multiplication((l, r)))),
+        Ok((_, (l, " / ", r))) => Ok(("", Division((l, r)))),
+        res => panic!("{:?}", res),
+    }
+}
+
 fn parse_rhs(input: &str) -> IResult<&str, Job> {
     alt((map(i64, Number), parse_operation))(input)
 }
-fn parse_operation(input: &str) -> IResult<&str, Job> {
-    alt((parse_add, parse_sub, parse_mul, parse_div))(input)
+
+fn parse_line(input: &str) -> IResult<&str, (String, Job)> {
+    separated_pair(parse_string, tag(": "), parse_rhs)(input)
 }
-fn parse_add(input: &str) -> IResult<&str, Job> {
-    map(
-        separated_pair(parse_string, tag(" + "), parse_string),
-        Addition,
-    )(input)
-}
-fn parse_sub(input: &str) -> IResult<&str, Job> {
-    map(
-        separated_pair(parse_string, tag(" - "), parse_string),
-        Subtraction,
-    )(input)
-}
-fn parse_mul(input: &str) -> IResult<&str, Job> {
-    map(
-        separated_pair(parse_string, tag(" * "), parse_string),
-        Multiplication,
-    )(input)
-}
-fn parse_div(input: &str) -> IResult<&str, Job> {
-    map(
-        separated_pair(parse_string, tag(" / "), parse_string),
-        Division,
-    )(input)
-}
+
 fn parse(input: &str) -> HashMap<String, Job> {
-    let (_, right): (Vec<_>, HashMap<_, _>) = input.lines().map(parse_line).flatten().unzip();
-    right
+    input.lines().map(|l| parse_line(l).unwrap().1).collect()
 }
