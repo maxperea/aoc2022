c710c8e6951bd98c3d19391b982f9f7d2823f008
Max Perea Düring <maxpereaduring@gmail.com>
2023-01-07 14:19:40 +0100

    Day22 part 2

--- src/days/day22.rs
+++ src/days/day22.rs
@@ -9,14 +9,27 @@ pub fn solution_easy(input: &str) -> usize {
         match instruction {
             Instruction::TurnLeft => state.turn_left(),
             Instruction::TurnRight => state.turn_right(),
-            Instruction::Step(n) => state.step(&map, n - 1, &state.pos.next(state.change(), &map)),
+            Instruction::Step(n) => {
+                state.step(&map, n - 1, &state.pos.next_wrap(state.change(), &map))
+            }
         }
-        // println!(
-        //     "Y: {}, X: {}, Face: {}",
-        //     state.pos.y,
-        //     state.pos.x,
-        //     state.face.to_num()
-        // );
+    }
+    (state.pos.y + 1) * 1000 + (state.pos.x + 1) * 4 + state.face.to_num()
+}
+
+pub fn solution_hard(input: &str) -> usize {
+    let map = parse_map(&input);
+    let instructions = parse_instructions(&input);
+    let mut state = State {
+        pos: Position { x: 50, y: 0 },
+        face: Direction::Right,
+    };
+    for instruction in instructions {
+        match instruction {
+            Instruction::TurnLeft => state.turn_left(),
+            Instruction::TurnRight => state.turn_right(),
+            Instruction::Step(n) => state.cube_step(&map, n),
+        }
     }
     (state.pos.y + 1) * 1000 + (state.pos.x + 1) * 4 + state.face.to_num()
 }
@@ -25,14 +38,41 @@ impl State {
     fn step(&mut self, map: &Map, count: usize, next: &Position) {
         match map[next.y][next.x] {
             Tile::Wall => return,
-            Tile::None => self.step(map, count, &next.next(self.change(), map)),
+            Tile::None => self.step(map, count, &next.next_wrap(self.change(), map)),
             Tile::Open => {
                 self.pos = next.clone();
                 if count == 0 {
                     return;
                 }
-                self.step(map, count - 1, &next.next(self.change(), map));
+                self.step(map, count - 1, &next.next_wrap(self.change(), map));
             }
+        }
+    }
+
+    fn cube_step(&mut self, map: &Map, count: usize) {
+        if count == 0 {
+            return;
+        }
+        if let Some(pos) = self.pos.next(self.change(), map) {
+            if map[pos.y][pos.x] == Tile::Wall {
+                return;
+            }
+            if map[pos.y][pos.x] == Tile::None {
+                panic!();
+            }
+            self.pos = pos;
+            self.cube_step(map, count - 1);
+        } else {
+            let (new_face, new_pos) = get_cube_step(self);
+            if map[new_pos.y][new_pos.x] == Tile::Wall {
+                return;
+            }
+            if map[new_pos.y][new_pos.x] == Tile::None {
+                panic!();
+            }
+            self.pos = new_pos;
+            self.face = new_face;
+            self.cube_step(map, count - 1);
         }
     }
 
@@ -53,23 +93,158 @@ impl State {
     }
 }
 
+fn get_cube_step(state: &State) -> (Direction, Position) {
+    match state.face {
+        Direction::Down => {
+            if state.pos.x < 50 {
+                (
+                    Direction::Down,
+                    Position {
+                        x: state.pos.x + 100,
+                        y: 0,
+                    },
+                )
+            } else if state.pos.x < 100 {
+                (
+                    Direction::Left,
+                    Position {
+                        x: 49,
+                        y: state.pos.x - 50 + 150,
+                    },
+                )
+            } else {
+                (
+                    Direction::Left,
+                    Position {
+                        x: 99,
+                        y: state.pos.x - 100 + 50,
+                    },
+                )
+            }
+        }
+        Direction::Up => {
+            if state.pos.x < 50 {
+                (
+                    Direction::Right,
+                    Position {
+                        x: 50,
+                        y: state.pos.x + 50,
+                    },
+                )
+            } else if state.pos.x < 100 {
+                (
+                    Direction::Right,
+                    Position {
+                        x: 0,
+                        y: state.pos.x - 50 + 150,
+                    },
+                )
+            } else {
+                (
+                    Direction::Up,
+                    Position {
+                        x: state.pos.x - 100,
+                        y: 199,
+                    },
+                )
+            }
+        }
+        Direction::Right => {
+            if state.pos.y < 50 {
+                (
+                    Direction::Left,
+                    Position {
+                        x: 99,
+                        y: 49 - state.pos.y + 100,
+                    },
+                )
+            } else if state.pos.y < 100 {
+                (
+                    Direction::Up,
+                    Position {
+                        x: state.pos.y - 50 + 100,
+                        y: 49,
+                    },
+                )
+            } else if state.pos.y < 150 {
+                (
+                    Direction::Left,
+                    Position {
+                        x: 149,
+                        y: 49 - (state.pos.y - 100),
+                    },
+                )
+            } else {
+                (
+                    Direction::Up,
+                    Position {
+                        x: state.pos.y - 150 + 50,
+                        y: 149,
+                    },
+                )
+            }
+        }
+        Direction::Left => {
+            if state.pos.y < 50 {
+                (
+                    Direction::Right,
+                    Position {
+                        x: 0,
+                        y: (49 - state.pos.y) + 100,
+                    },
+                )
+            } else if state.pos.y < 100 {
+                (
+                    Direction::Down,
+                    Position {
+                        x: state.pos.y - 50,
+                        y: 100,
+                    },
+                )
+            } else if state.pos.y < 150 {
+                (
+                    Direction::Right,
+                    Position {
+                        x: 50,
+                        y: 49 - (state.pos.y - 100),
+                    },
+                )
+            } else {
+                (
+                    Direction::Down,
+                    Position {
+                        x: state.pos.y - 150 + 50,
+                        y: 0,
+                    },
+                )
+            }
+        }
+    }
+}
+
 impl Position {
-    fn next(&self, (dx, dy): (i32, i32), map: &Map) -> Position {
+    fn next_wrap(&self, (dx, dy): (i32, i32), map: &Map) -> Position {
         Position {
             x: ((self.x as i32 + dx).rem_euclid(map[0].len() as i32)) as usize,
             y: ((self.y as i32 + dy).rem_euclid(map.len() as i32)) as usize,
         }
     }
-}
 
-pub fn solution_hard(input: &str) -> i64 {
-    let _data = parse(input);
-    unimplemented!()
-}
-
-fn parse(_input: &str) -> i64 {
-    let _re = regex::Regex::new(r"\d+").unwrap();
-    unimplemented!()
+    fn next(&self, (dx, dy): (i32, i32), map: &Map) -> Option<Position> {
+        if self.x as i32 + dx < 0 || self.x as i32 + dx >= map[0].len() as i32 {
+            return None;
+        }
+        if self.y as i32 + dy < 0 || self.y as i32 + dy >= map.len() as i32 {
+            return None;
+        }
+        if map[(self.y as i32 + dy) as usize][(self.x as i32 + dx) as usize] == Tile::None {
+            return None;
+        }
+        Some(Position {
+            x: (self.x as i32 + dx) as usize,
+            y: (self.y as i32 + dy) as usize,
+        })
+    }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -86,7 +261,7 @@ enum Instruction {
     Step(usize),
 }
 
-#[derive(rotate_enum::RotateEnum, Clone, Copy)]
+#[derive(rotate_enum::RotateEnum, Clone, Copy, Debug)]
 enum Direction {
     Right,
     Down,
